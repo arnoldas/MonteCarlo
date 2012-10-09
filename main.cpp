f3b739635d7b629881ae0d029b6f6a9a7baf0a87
Arnoldas Rope <arnoldas.rope@gmail.com>
2012-10-09 17:38:59 +0200

    Pridetas kintamasis xSprend

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
     cout << "MonteCarlo metodo realizacijos pradzia!" << endl;
+    cout << "---RANDOM SEARCH---" << endl;
     // Kintamuju apsirasymas
-    double f, x;  // Funkcijos f reiksmes ir argumentas x
-    int n;        // Uzdavinio dimensija
+    double f, x;    // Funkcijos f reiksmes ir argumentas x
+    int n;          // Uzdavinio dimensija
+    double xSprend; // Funkcijos x sprendinys
     return 0;
 }
