bca94987c29b74d5fee63cfb67814f44c3804301
Arnoldas Ropė <arnoldas.rope@gmail.com>
2012-11-19 00:54:25 +0200

    Realizuoti abu sprendimo budai(3 ir 4) su vektoriais

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <vector>
+#include <algorithm>
+#define N 100
 // funkcija  http://www.it.lut.fi/ip/evo/functions/node26.html
 // six camel hump function
 using namespace std;
@@ -34,7 +37,7 @@ void insertionSort(taskai *arr, int length) {
 
 void pirmas_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2)
 {
-    int mas_dydis = 100;
+    int mas_dydis = N;
     int n;//dimensija
     taskai *taskuArray = new taskai[mas_dydis];
 
@@ -72,7 +75,7 @@ void pirmas_budas(double p_apacia, double p_virsus, double p_apacia2, double p_v
 
 void antras_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2, double p_fSprend, double p_epsilon)
 {
-    int mas_dydis=100;
+    int mas_dydis=N;
     taskai *taskuArray = new taskai[mas_dydis];
     int ilgis=-1;
     cout<<"===Sprendziame antru budu===\n";
@@ -106,7 +109,58 @@ void antras_budas(double p_apacia, double p_virsus, double p_apacia2, double p_v
     }
     cout<<"Darbas baigtas\n";
     delete []taskuArray;
+}
 
+bool rusiavimas(const taskai a, taskai b)
+{
+    return a.sprendinys<b.sprendinys;
+}
+
+void trecias_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2)
+{
+    cout<<"===Sprendziame treciu budu===\n";
+    vector <taskai> vec(0);
+    taskai tmptaskai;
+    int n;
+    cout<<"Iveskite uzdavinio dimensija(n):";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        tmptaskai.x1=rand() * (p_virsus - p_apacia) / RAND_MAX + p_apacia;
+        tmptaskai.x2=rand() * (p_virsus2 - p_apacia2) / RAND_MAX + p_apacia2;
+        tmptaskai.sprendinys=sixhump(tmptaskai.x1,tmptaskai.x2);
+        vec.push_back(tmptaskai);
+        cout<<i<<'\t'<<vec[i].x1<<'\t'<<vec[i].x2<<'\t'<<vec[i].sprendinys<<endl;
+    }
+    sort(vec.begin(),vec.end(),rusiavimas);
+    cout<<"\nSurusiuotas vektorius pagal funkcijos sprendini(TOP 5)\n";
+    for(int j=0;j<5;j++)
+        cout<<j<<'\t'<<vec[j].x1<<'\t'<<vec[j].x2<<'\t'<<vec[j].sprendinys<<endl;
+
+    cout<<"Darbas baigtas\n";
+}
+
+void ketvirtas_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2, double p_fSprend, double p_epsilon)
+{
+    cout<<"===Sprendziame ketvirtu budu===\n";
+    vector <taskai> vec(0);
+    int ilgis=-1;
+    taskai tmptaskai;
+    do
+    {
+        ilgis++;
+        tmptaskai.x1=rand() * (p_virsus - p_apacia) / RAND_MAX + p_apacia;
+        tmptaskai.x2=rand() * (p_virsus2 - p_apacia2) / RAND_MAX + p_apacia2;
+        tmptaskai.sprendinys=sixhump(tmptaskai.x1,tmptaskai.x2);
+        vec.push_back(tmptaskai);
+        cout<<ilgis<<'\t'<<vec[ilgis].x1<<'\t'<<vec[ilgis].x2<<'\t'<<vec[ilgis].sprendinys<<endl;
+    }
+    while(fabs(p_fSprend-(vec[ilgis].sprendinys))>p_epsilon);
+    sort(vec.begin(),vec.end(),rusiavimas);
+    cout<<"\nSurusiuotas vektorius pagal funkcijos sprendini(TOP 5)\n";
+    for(int j=0;j<5;j++)
+        cout<<j<<'\t'<<vec[j].x1<<'\t'<<vec[j].x2<<'\t'<<vec[j].sprendinys<<endl;
+    cout<<"Darbas baigtas\n";
 }
 
 int main()
@@ -126,14 +180,21 @@ int main()
     virsutinis_rezis = 1.9;
     apatinis_rezis2 = -1.1;
     virsutinis_rezis2 = 1.1;
-    cout << "Pasirinkite minimumo ieskojimo buda(1/2): ";
+    cout << "Pasirinkite minimumo ieskojimo buda(1/2/3/4): ";
     cin >> budas;
-    if(budas==1)
-        pirmas_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2);
-    else if(budas==2)
-        antras_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2, fSprendinys, epsilon);
-    else
-      cout << "Nekorektiskai pasirinktas sprendimo budas.";
+    switch(budas)
+    {
+        case 1:pirmas_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2);
+        break;
+        case 2:antras_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2, fSprendinys, epsilon);
+        break;
+        case 3:trecias_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2);
+        break;
+        case 4:ketvirtas_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2, fSprendinys, epsilon);
+        break;
+        default:cout << "Nekorektiskai pasirinktas sprendimo budas.";
+        break;
+    }
 
     return 0;
 }
