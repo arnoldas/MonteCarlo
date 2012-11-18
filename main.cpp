01d7ce788f2d7dba778050cf9f5c8760b476a8e7
Arnoldas Ropė <arnoldas.rope@gmail.com>
2012-11-18 19:51:32 +0200

    Pilnai realizuotas funkcionalumas su dinaminem strukturom

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ double sixhump(double x0, double x1)
     return f;
 }
 
-void insertionSort(taskai *arr/*int arr[]*/, int length) {
+void insertionSort(taskai *arr, int length) {
       int i, j;
       taskai tmp;
       for (i = 1; i < length; i++) {
@@ -33,8 +33,8 @@ void insertionSort(taskai *arr/*int arr[]*/, int length) {
 }
 
 void pirmas_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2)
-{   //susikuriam strukturu masyva
-    int mas_dydis = 1;//iteraciju skaicius, kazkodel kai yra struktura, ji pati issiskiria kiek jai reikia vietos
+{
+    int mas_dydis = 100;
     int n;//dimensija
     taskai *taskuArray = new taskai[mas_dydis];
 
@@ -43,6 +43,18 @@ void pirmas_budas(double p_apacia, double p_virsus, double p_apacia2, double p_v
     cin>>n;
     for(int i=0;i<n;i++)
     {
+        if((i+1)==mas_dydis)
+        {
+            cout<<"Esam ife\n";
+            taskai *tmptaskuArray = taskuArray;
+            mas_dydis=mas_dydis*2;
+            taskuArray = new taskai[mas_dydis];
+            for(int j=0;j<i;j++)
+                taskuArray[j]=tmptaskuArray[j];
+
+            delete []tmptaskuArray;
+
+        }
         taskuArray[i].x1=rand() * (p_virsus - p_apacia) / RAND_MAX + p_apacia;
         taskuArray[i].x2=rand() * (p_virsus2 - p_apacia2) / RAND_MAX + p_apacia2;
         taskuArray[i].sprendinys=sixhump(taskuArray[i].x1,taskuArray[i].x2);
@@ -55,11 +67,46 @@ void pirmas_budas(double p_apacia, double p_virsus, double p_apacia2, double p_v
         cout<<i<<'\t'<<taskuArray[i].x1<<'\t'<<taskuArray[i].x2<<'\t'<<taskuArray[i].sprendinys<<endl;
     }
     cout<<"Darbas baigtas\n";
+    delete []taskuArray;
 }
 
-void antras_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2)
+void antras_budas(double p_apacia, double p_virsus, double p_apacia2, double p_virsus2, double p_fSprend, double p_epsilon)
 {
+    int mas_dydis=100;
+    taskai *taskuArray = new taskai[mas_dydis];
+    int ilgis=-1;
     cout<<"===Sprendziame antru budu===\n";
+    do
+    {
+        if((ilgis+1)==mas_dydis)
+        {
+            cout<<"Esam ife\n";
+            taskai *tmptaskuArray = taskuArray;
+            mas_dydis=mas_dydis*2;
+            cout<<"MAS_DYDIS="<<mas_dydis<<endl;
+            taskuArray = new taskai[mas_dydis];
+            for(int i=0;i<ilgis;i++)
+                taskuArray[i]=tmptaskuArray[i];
+
+            delete []tmptaskuArray;
+
+        }
+        ilgis++;
+        taskuArray[ilgis].x1=rand() * (p_virsus - p_apacia) / RAND_MAX + p_apacia;
+        taskuArray[ilgis].x2=rand() * (p_virsus2 - p_apacia2) / RAND_MAX + p_apacia2;
+        taskuArray[ilgis].sprendinys=sixhump(taskuArray[ilgis].x1,taskuArray[ilgis].x2);
+        cout<<ilgis<<'\t'<<taskuArray[ilgis].x1<<'\t'<<taskuArray[ilgis].x2<<'\t'<<taskuArray[ilgis].sprendinys<<endl;
+    }
+    while(fabs(p_fSprend-(taskuArray[ilgis].sprendinys))>p_epsilon);
+    insertionSort(taskuArray, ilgis+1);
+    cout<<"\nSurusiuotas masyvas pagal funkcijos sprendini(TOP 5)\n";
+    for(int i=0;i<5;i++)
+    {
+        cout<<i<<'\t'<<taskuArray[i].x1<<'\t'<<taskuArray[i].x2<<'\t'<<taskuArray[i].sprendinys<<endl;
+    }
+    cout<<"Darbas baigtas\n";
+    delete []taskuArray;
+
 }
 
 int main()
@@ -68,7 +115,7 @@ int main()
     cout << "(RANDOM SEARCH METHOD)\n";
     //Kintamuju apsirasymas
     double fSprendinys=-1.031628453; // Uzdavinio sprendinys
-    double epsilon=0.001;  //paklaida
+    double epsilon=0.0001;  //paklaida 0.01 - 2.4s; 0.001 - 2.4s; 0.0001 - 2min
     int budas; //Uzdavinio sprendimo budas
     double virsutinis_rezis, apatinis_rezis, virsutinis_rezis2, apatinis_rezis2; // apsirasome intervalo rezius
     //cout << "Iveskite inervalo rerzius x1: ";
@@ -84,81 +131,9 @@ int main()
     if(budas==1)
         pirmas_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2);
     else if(budas==2)
-        antras_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2);
+        antras_budas(apatinis_rezis,virsutinis_rezis,apatinis_rezis2,virsutinis_rezis2, fSprendinys, epsilon);
     else
       cout << "Nekorektiskai pasirinktas sprendimo budas.";
 
     return 0;
-/*  double f, x; // Funkcijos reiksmes ir argumentas x
-    int n; // Uzedavinio dimensija
-    // Vektoriu kurimas, kuriu elementai yra atitiktiniai realus skaiciai
-
-    if(budas==1)
-    {
-        cout << "Iveskite vektoriaus dimensija (n):";
-        cin >> n;
-
-        double vektorius[n], funkcija[200];
-
-        for (int j=0; j<100; j++)
-        {
-
-            for(int i=0; i<n; i++)
-            {
-                if (i == 0)
-                {
-                    vektorius[i] = rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
-                    cout << "Skaicius = " << vektorius[i] << endl; // pakeiciau
-                }
-                else
-                {
-                    vektorius[i] = rand() * (virsutinis_rezis2 - apatinis_rezis2) / RAND_MAX + apatinis_rezis2;
-                    cout << "Skaicius = " << vektorius[i] << endl; // pakeiciau
-                }
-
-            }
-
-            cout << "Funkcija = " << sixhump(&vektorius[0]) << endl; // pakeiciau
-            cout << "===================================" << endl;
-            funkcija[j] = sixhump(&vektorius[0]);
-        }
-
-        double min = funkcija[0];
-        for (int i=0; i<100; i++)
-        {
-            if (funkcija[i] < min) min = funkcija[i];
-        }
-        cout << "MIN: " << min;
-    }
-    else if(budas==2)
-    {
-
-        rezultatai *r1=NULL, *r2=NULL;
-
-        do
-        {
-            if(r1==NULL)
-            {
-                r1=(rezultatai *)malloc(sizeof(rezultatai));
-                r2=r1;
-            }
-            else
-            {
-                r2->kitas=(rezultatai *)malloc(sizeof(rezultatai));
-                r2=r2->kitas;
-            }
-            r2->x1=rand() * (virsutinis_rezis - apatinis_rezis) / RAND_MAX + apatinis_rezis;
-            r2->x2=rand() * (virsutinis_rezis2 - apatinis_rezis2) / RAND_MAX + apatinis_rezis2;
-            r2->fspr=(4-2.1*r2->x1 * r2->x1 + (pow(r2->x1,4))/3) * r2->x1 * r2->x1 + r2->x1 * r2->x2 + (-4+4*r2->x2*r2->x2)*(pow(r2->x2,2));
-            r2->kitas=NULL;
-            cout<<"X1="<<r2->x1<<" X2="<<r2->x2<<" sprend="<<r2->fspr<<endl;
-        }
-        while(fabs(fSprendinys-(r2->fspr))>epsilon);
-        //reikia parasyt while(f_globalus-fmin)<epsilon(0.01 poto 0.001)
-        cout<<"---------------------------------------------------------"<<endl;
-        cout<<"X1="<<r2->x1<<" X2="<<r2->x2<<" sprend="<<r2->fspr<<endl;
-    }
-    else
-        cout<<"Nekorektiðkai pasirinktas sprendimo budas.";
-    return 0;*/
 }
